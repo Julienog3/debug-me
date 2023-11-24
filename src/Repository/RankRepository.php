171d80f1df573c621df21562bc23baa5a69c7cf4
LeonardPaillet <Tactic22paill@gmail.com>
2023-11-24 12:14:19 +0100

    repo

---
+++ src/Repository/RankRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Rank;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Rank>
+ *
+ * @method Rank|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Rank|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Rank[]    findAll()
+ * @method Rank[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class RankRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Rank::class);
+    }
+
+//    /**
+//     * @return Rank[] Returns an array of Rank objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('r')
+//            ->andWhere('r.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('r.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Rank
+//    {
+//        return $this->createQueryBuilder('r')
+//            ->andWhere('r.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
