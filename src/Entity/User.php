4a10c2443e637da4650a83a70f878446ec872ead
LeonardPaillet <Tactic22paill@gmail.com>
2023-11-24 12:09:15 +0100

    Register form & Login form

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,6 +12,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[ORM\HasLifecycleCallbacks]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -45,6 +46,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(length: 25)]
     private ?string $firstname = null;
+
+    #[ORM\PrePersist]
+    public function setCreatedAtValue(): void
+    {
+        $this->created_at = new \DateTime();
+    }
+    #[ORM\PrePersist]
+    public function setActivityPointAtValue(): void
+    {
+        $this->activity_point = 0;
+    }
+
 
     public function getId(): ?int
     {
